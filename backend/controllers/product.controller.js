444bf8a690b7805655b8bb6d6870c1bbb19ec3fb
gurdnarbextar <gurdnarbextar@gmail.com>
2025-01-08 22:05:29 +0000

    #

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
+// Get all products
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find({});
@@ -11,13 +12,15 @@ export const getProducts = async (req, res) => {
   }
 };
 
+// Create a new product
 export const createProduct = async (req, res) => {
   const product = req.body; // user will send this data
 
+  // Validate fields
   if (!product.name || !product.price || !product.image) {
     return res
       .status(400)
-      .json({ sucess: false, message: "All fields are required" });
+      .json({ success: false, message: "All fields are required" });
   }
 
   const newProduct = new Product(product);
@@ -31,34 +34,52 @@ export const createProduct = async (req, res) => {
   }
 };
 
+// Update an existing product
 export const updateProduct = async (req, res) => {
   const { id } = req.params;
   const product = req.body;
 
+  // Check if the provided id is valid
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).jason({ sucess: false, message: "InValid Id" });
+    return res.status(400).json({ success: false, message: "Invalid Id" });
   }
 
   try {
     const updatedProduct = await Product.findByIdAndUpdate(id, product, {
       new: true,
     });
-    res.status(200).json({ sucess: true, data: updatedProduct });
+
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: updatedProduct });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Product not found" });
+    console.log("Error in Updating product:", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
+// Delete a product
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
   try {
-    await Product.findByIdAndDelete(id);
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
     res
       .status(200)
       .json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
     console.log("Error in Deleting product:", error.message);
-    res.status(404).json({ success: false, message: "Product not found" });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
